7307042278d5361f2c35ffe6eb396c86425dd5b6
cesar9898 <graficacevallos@gmail.com>
2025-05-28 07:00:43 +0000

    nuevos cambios CRUD

--- apicopimanta/src/main/java/com/ordenesproduccion/apicopimanta/controladores/OrdenesProduccionRestController.java
+++ apicopimanta/src/main/java/com/ordenesproduccion/apicopimanta/controladores/OrdenesProduccionRestController.java
@@ -31,18 +31,19 @@ public class OrdenesProduccionRestController {
         List<Map<String, Object>> trabajos = new ArrayList<>();
         Map<String, Object> trabajo1 = new HashMap<>();
         trabajo1.put("tipo", "Tarjetas de presentación");
-        trabajo1.put("cantidad", 500);
+        trabajo1.put("cantidad", 1000);
         trabajo1.put("color", "Full color");
 
         Map<String, Object> trabajo2 = new HashMap<>();
-        trabajo2.put("tipo", "Folletos");
-        trabajo2.put("cantidad", 1000);
-        trabajo2.put("color", "Blanco y negro");
+        trabajo2.put("tipo", "Flayers promocionales");
+        trabajo2.put("cantidad", 5000);
+        trabajo2.put("color", "full color");
 
         trabajos.add(trabajo1);
         trabajos.add(trabajo2);
 
         // Orden de producción 
+        respuesta.put("ordenId", "456");
         respuesta.put("cliente", cliente);
         respuesta.put("trabajos", trabajos);
         respuesta.put("estado", "Creada");
@@ -50,6 +51,47 @@ public class OrdenesProduccionRestController {
 
         return respuesta;
     }
+       // endpoint para enlistar los estados y su ID de las órdenes de producción
+    @GetMapping("/estados")
+    public List<Map<String, Object>> obtenerEstados() {
+        List<Map<String, Object>> estados = new ArrayList<>();
+
+        Map<String, Object> estado1 = new HashMap<>();
+        estado1.put("id", 1);
+        estado1.put("nombre", "Pendiente");
+
+        Map<String, Object> estado2 = new HashMap<>();
+        estado2.put("id", 2);
+        estado2.put("nombre", "En proceso");
+
+        Map<String, Object> estado3 = new HashMap<>();
+        estado3.put("id", 3);
+        estado3.put("nombre", "Completada");
+
+        estados.add(estado1);
+        estados.add(estado2);
+        estados.add(estado3);
+
+        return estados;
+   
+    }
+        // Endpoint para actualizar datos de una orden de producción
+    @PutMapping("/ordenes/{id}")
+    public Map<String, Object> actualizarOrden(@PathVariable String id, @RequestBody OrdenesDto ordenActualizada) {
+        Map<String, Object> respuesta = new HashMap<>();
+        
+        // Simulación de actualización de la orden
+        respuesta.put("ordenId", id);
+        respuesta.put("cliente", ordenActualizada.getCliente());
+        respuesta.put("trabajo", ordenActualizada.getTrabajo());
+        respuesta.put("cantidad", ordenActualizada.getCantidad());
+        respuesta.put("estado", ordenActualizada.getEstado());
+        respuesta.put("fechaActualizacion", new Date());
+
+        return respuesta;
+    }
+    
+
 
 
 }
