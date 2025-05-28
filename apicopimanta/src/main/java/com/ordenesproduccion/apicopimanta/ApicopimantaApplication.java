ff34b505afeb1ca9397ab2269768145efb2dd192
cesar9898 <graficacevallos@gmail.com>
2025-05-28 05:47:04 +0000

    creacion de api imprenta copimanta

---
+++ apicopimanta/src/main/java/com/ordenesproduccion/apicopimanta/ApicopimantaApplication.java
@@ -0,0 +1,13 @@
+package com.ordenesproduccion.apicopimanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicopimantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicopimantaApplication.class, args);
+	}
+
+}
---
+++ apicopimanta/src/test/java/com/ordenesproduccion/apicopimanta/ApicopimantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordenesproduccion.apicopimanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicopimantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
